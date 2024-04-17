1c7a6da2b6579f911b3a93ae709d0c804bb5c364
sergeybabann <sergeybabann@yandex.ru>
2024-04-17 12:24:40 +0300

    additions

---
+++ main_sections/react/bogdan_stashchuk/full_course/08.adding_state_a_properties_for_a_component/62.you_cannot_overwrite_the_values_of_variables_from_the_indirect_state/02_example/script.js
@@ -0,0 +1,16 @@
+const App = () => {
+    let [buttonText, setButtonText] = React.useState('Click me')
+    
+    const onButtonClick = () => {
+        buttonText = 'Hello from React'  
+}
+    return (
+        <div className="app">
+            <button onClick={onButtonClick}>{buttonText}</button>
+        </div>
+    )
+}
+
+const container = document.getElementById('app')
+const root = ReactDOM.createRoot(container)
+root.render(<App />)
---
+++ main_sections/react/bogdan_stashchuk/full_course/08.adding_state_a_properties_for_a_component/63.changing_state_using_a_function_call/02_example/script.js
@@ -0,0 +1,17 @@
+const App = () => {
+    console.log('Called')
+    let [buttonText, setButtonText] = React.useState('Click me')
+    
+    const onButtonClick = () => {
+        setButtonText = ('Hello from React')  
+}
+    return (
+        <div className="app">
+            <button onClick={onButtonClick}>{buttonText}</button>
+        </div>
+    )
+}
+
+const container = document.getElementById('app')
+const root = ReactDOM.createRoot(container)
+root.render(<App />)
